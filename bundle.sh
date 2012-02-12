d324107b68ef278cea4eccd1b97b13477f1c5d29
Suraj N. Kurapati <sunaku@gmail.com>
2012-02-11 23:04:56 -0800

    master branch: bare bones for anyone to start with

---
+++ bundle.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Clones or updates the Git repositories specified in `./**/*.get` files,
+# starting from the most recently modified file down to the earliest one.
+
+find . -type f -name '*.get' -print | xargs ls -t | while read get; do
+  url=$(cat "$get")
+  dir=${get%.get}
+  echo -n "$dir => "
+  if cd "$dir/.git/.." 2>/dev/null; then
+    git remote set-url origin "$url"
+    git pull origin
+    cd - >/dev/null
+  else
+    git clone "$url" "$dir"
+  fi
+done
